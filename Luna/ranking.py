5ad8c5f7904d8662cfad2aaba551d82d85d5789f
chancy <chanchenyang32@gmail.com>
2024-09-09 14:49:53 +0800

    updated ranking.py

--- Luna/ranking.py
+++ Luna/ranking.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def rank_pred(model, features, names): # features input refers to resultant df after calling get_features function, names input refers to corresponding names after calling get_names 
+def rank_pred(model, features): # features input refers to resultant df after calling get_features function, names input refers to corresponding names after calling get_names 
     probs = model.predict_proba(features)[:, 1] 
 
     df_probs = pd.DataFrame(features.copy())
@@ -12,9 +12,26 @@ def rank_pred(model, features, names): # features input refers to resultant df a
     new_order = columns[-1:] + columns[:-1] 
     df_sorted = df_sorted[new_order]
 
-    df_merged = names.join(df_sorted, how='inner')  # join on the row index
-    df_merged = df_merged.reindex(df_sorted.index)
-    return df_merged
+    #df_merged = names.join(df_sorted, how='inner')  # join on the row index
+    #df_merged = df_merged.reindex(df_sorted.index)
+    return df_sorted
+
+
+def custom_rank_pred(model, features, criteria): # features input refers to resultant df after calling get_features function, names input refers to corresponding names after calling get_names 
+    probs = model.predict_proba(features)[:, 1] 
+
+    df_probs = pd.DataFrame(features.copy())
+    df_probs['Prediction Prob'] = probs
+
+    df_sorted = df_probs.sort_values(by=criteria, ascending=False)
+
+    columns = df_sorted.columns.tolist()
+    new_order = columns[-1:] + columns[:-1] 
+    df_sorted = df_sorted[new_order]
+
+    #df_merged = names.join(df_sorted, how='inner')  # join on the row index
+    #df_merged = df_merged.reindex(df_sorted.index)
+    return df_sorted
 
 def get_names(df): # df refers to exensio dataset with test parameter names (before feature processing)
     return df.loc['Name'] 
